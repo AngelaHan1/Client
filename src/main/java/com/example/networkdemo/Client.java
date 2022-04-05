60d12b450fcddfa06d34c128ec8a63dbf93ed8af
Van Pham <thyvan1412@gmail.com>
2022-04-04 20:53:48 -0700

    Added 2 threads

--- src/main/java/com/example/networkdemo/Client.java
+++ src/main/java/com/example/networkdemo/Client.java
@@ -6,10 +6,7 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -19,12 +16,18 @@ public class Client extends Application {
     DataInputStream fromServer = null;
 
     @Override // Override the start method in the Application class
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+
+        // Button to send message.
+        Button sendButton = new Button("send");
+        sendButton.setDefaultButton(true);
+
         // Panel p to hold the label and text field
         BorderPane paneForTextField = new BorderPane();
         paneForTextField.setPadding(new Insets(5, 5, 5, 5));
         paneForTextField.setStyle("-fx-border-color: green");
-        paneForTextField.setLeft(new Label("Enter a message: "));
+        paneForTextField.setLeft(new Label("Enter message: "));
+        paneForTextField.setRight(sendButton);
 
         TextField tf = new TextField();
         tf.setAlignment(Pos.BOTTOM_RIGHT);
@@ -34,7 +37,7 @@ public class Client extends Application {
         // Text area to display contents
         TextArea ta = new TextArea();
         mainPane.setCenter(new ScrollPane(ta));
-        mainPane.setTop(paneForTextField);
+        mainPane.setBottom(paneForTextField);
 
         // Create a scene and place it in the stage
         Scene scene = new Scene(mainPane, 450, 200);
@@ -42,28 +45,73 @@ public class Client extends Application {
         primaryStage.setScene(scene); // Place the scene in the stage
         primaryStage.show(); // Display the stage
 
-        tf.setOnAction(e -> {
-            try {
-                // Get the radius from the text field
-                String message = tf.getText().trim();
+        sendButton.setOnAction(e -> {
 
-                System.out.println(message);
+                /*String message;
 
-                // Send the message to the server
-                toServer.writeUTF(message);
-                toServer.flush();
+                    // Get the radius from the text field
+                    message = tf.getText().trim();
 
-                // Get message from the server
-                message = fromServer.readUTF();
+                    System.out.println(message);
 
-                // Display to the text area
-                ta.appendText(message + "\n");
+                    // Send the message to the server
+                    toServer.writeUTF(message);
+                    toServer.flush();
 
-            }
-            catch (IOException ex) {
-                System.err.println(ex);
+                    // Clear text field area
+                    tf.setText("");
+
+                    // Get message from the server
+                    message = fromServer.readUTF();
+                    // Display to the text area
+                    ta.appendText(message + "\n"); */
+                // sendMessage thread
+                Thread sendMessage = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                            try {
+                                // read the message to deliver.
+                                System.out.println("Start print message");
+                                String msg = tf.getText().trim();
+                                // write on the output stream
+                                toServer.writeUTF(msg);
+                                //toServer.flush();
+
+                                // Clear text field area
+                                tf.setText("");
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                });
+
+                sendMessage.start();
+        });
+
+        // readMessage thread
+        Thread readMessage = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                while (true) {
+                    try {
+                        System.out.println("Start read msg");
+                        // read the message sent to this client
+                        if (fromServer != null)
+                        {
+                            String msg = fromServer.readUTF();
+                            System.out.println(msg);
+
+                            ta.appendText(msg + "\n");
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
+
+        readMessage.start();
 
         try {
             // Create a socket to connect to the server
@@ -76,10 +124,12 @@ public class Client extends Application {
 
             // Create an output stream to send data to the server
             toServer = new DataOutputStream(socket.getOutputStream());
+
         }
         catch (IOException ex) {
             ta.appendText(ex.toString() + '\n');
         }
+
     }
 
     /**
